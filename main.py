ab4cb7f5741cd8a2264be51b15f92074179caa05
Patryk Peczak <ppeczak0405@gmail.com>
2021-06-23 20:32:27 +0200

    Add assets

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import pygame
+import os
+
+BACKGROUND = pygame.image.load(os.path.join("assets", "background-black.png"))
+# Enemy ships
+RED_ENEMY_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.png"))
+GREEN_ENEMY_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_green_small"))
 
 if __name__ == '__main__':
     print("SpaceInvaders!")
